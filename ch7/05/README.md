4eba6fe124b8f3824f806c1160c05628a0d74637
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-04-02 14:11:45 +0200

    dod rozw zad 5

---
+++ ch7/05/README.md
@@ -0,0 +1,32 @@
+# Java - programming exercises
+
+# Info
+
+I'm solving programming exercises that are placed at the end of each chapter in:
+"Java in Two Semesters. Fourth edition" by Quentin Charatan and Aaron Kans
+
+Starting from Chapter 5 (other chapters' exercises were too easy)
+
+# Programming exercises from chapter 7 - Classes and Objects
+
+> In order to tackle these exercises make sure that the classes Oblong, Bank-
+> Account and EasyScanner have been copied from the website and placed in
+> the correct directory for your compiler to access them.
+>
+>
+>
+> 5. Write a program that creates an array of Oblong objects to represent the
+> dimensions of rooms in an apartment as described in self test question 6. The
+> program should allow the user to:
+>
+> * Determine the number of rooms;
+> * Enter the dimensions of the rooms;
+> * Retrieve the area and dimensions of any of the rooms.
+
+# Running program
+
+E.g.
+
+```bash
+java Rooms.java
+```
---
+++ ch7/05/Rooms.java
@@ -0,0 +1,85 @@
+// built-ins import
+import java.util.Scanner;
+
+// custom defined classes import
+import classes.Oblong;
+
+// the program does not check for the input corectness
+// (unless reqired in the task specification)
+public class Rooms {
+
+    private static Oblong aksUsrForSingleRoomDims() {
+       Scanner kbd = new Scanner(System.in);
+       double length, width;
+
+       System.out.println("Enter width of the room");
+       width = kbd.nextDouble();
+       System.out.println("Enter length of the room");
+       length = kbd.nextDouble();
+
+       // oblong accepts lengthIn and heightIn
+       return new Oblong(width, length);
+     }
+
+    private static Oblong[] askUsrForRoomsDims(int nOfRooms) {
+	Oblong[] rooms;
+	rooms = new Oblong[nOfRooms];
+
+	for (int i = 0; i < nOfRooms; i++) {
+	    System.out.printf("--Room %d:%n", i+1);
+	    rooms[i] = aksUsrForSingleRoomDims();
+	}
+
+	System.out.println("Dimentions of all the rooms have been entered");
+
+	return rooms;
+     }
+
+    private static void displayRoomInfo(Oblong roomIn, boolean shortVersion) {
+	if(shortVersion) {
+	    System.out.printf("Dims: %.2f [m] x %.2f[m]%n",
+			      roomIn.getLength(), roomIn.getHeight());
+	} else {
+	    // i added perimeter (not required in the task specification)
+	    System.out.printf("Dims: %.2f [m] x %.2f[m], " +
+			      "area: %.2f [m^2], " +
+			      "perim: %.2f [m]%n",
+			      roomIn.getLength(), roomIn.getHeight(),
+			      roomIn.calculateArea(),
+			      roomIn.calculatePerimeter());
+	}
+    }
+
+    private static void displayRoomsInfo(Oblong [] roomsIn,
+					 boolean shortVersion) {
+	for (int i = 0; i < roomsIn.length; i++) {
+	    System.out.println("--Room " + (i+1) + ":");
+	    displayRoomInfo(roomsIn[i], shortVersion);
+	}
+     }
+
+   public static void main(String[] args) {
+       // variables
+       Oblong[] rooms;
+       int numOfRooms = 0;
+       int roomId = 0;
+       Scanner kbd = new Scanner(System.in);
+
+       // program execution
+       System.out.println("Wellcome to the room creator.\n");
+
+       System.out.println("How many rooms do you want to create?");
+       numOfRooms = kbd.nextInt();
+       rooms = askUsrForRoomsDims(numOfRooms);
+
+       System.out.printf("%nThe following rooms are available%n");
+       displayRoomsInfo(rooms, true);
+
+       System.out.println("\nEnter a room number to get full info about it:");
+       roomId = kbd.nextInt();
+       System.out.println("Room " + (roomId) + ":");
+       displayRoomInfo(rooms[roomId - 1], false);
+
+       System.out.println("\nThat's all. Goodbye!");
+   }
+}
