22009626380184d0eac22a24630d1193dbce85bc
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-08-28 11:43:33 +0200

    dod. wym. pliki (kopiuj-wklej + modyf) Bank.java i BankAccount.java

---
+++ ch8/06/Bank.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+
+public class Bank {
+
+	ArrayList<BankAccount> list = new ArrayList<>();
+
+	// helper method to find the index of a specified account
+	private int search(String accountNumberIn) {
+		for (int i = 0; i <= list.size() - 1; i++) {
+			// find the account at index i
+			BankAccount tempAccount = list.get(i);
+			// get account number
+			String tempNumber = tempAccount.getAccountNumber();
+			// if this is the account we are looking for
+			if (tempNumber.equals(accountNumberIn)) {
+				return i; // return the index
+			}
+		}
+		return -999;
+	}
+
+	// return the total number of items
+	public int getTotal() {
+		return list.size();
+	}
+
+	// return an account with a particular account number
+	public BankAccount getItem(String accountNumberIn) {
+		int index = search(accountNumberIn);
+		// check that account exists
+		if (index != -999) {
+			return list.get(index);
+		} else {
+			return null; // no such account
+		}
+	}
+
+	// add an item to the list
+	public boolean addAccount(String accountNumberIn, String nameIn) {
+		// check that account does not already exist
+		if (search(accountNumberIn) == -999) {
+			// add new account
+			list.add(new BankAccount(accountNumberIn, nameIn));
+			return true;
+		}
+		return false;
+	}
+
+	// deposit money in a specified account
+	public boolean depositMoney(String accountNumberIn, double amountIn) {
+		BankAccount acc = getItem(accountNumberIn);
+		if (acc != null) {
+			acc.deposit(amountIn);
+			return true; // indicate success
+		} else {
+			return false; // indicate failure
+		}
+	}
+
+	// withdraw money from a specified account
+	public boolean withdrawMoney(String accountNumberIn, double amountIn) {
+		BankAccount acc = getItem(accountNumberIn);
+		if (acc != null && acc.getBalance() >= amountIn) {
+			acc.withdraw(amountIn);
+			return true; // indicate success
+		} else {
+			return false; // indicate failure
+		}
+	}
+
+	// remove an account
+	public boolean removeAccount(String accountNumberIn) {
+		int index = search(accountNumberIn); // find index of account
+		// if account exists account
+		if (index != -999) {
+			list.remove(index);
+			return true; // remove was successful
+		} else {
+			return false; // remove was unsuccessful
+		}
+	}
+}
